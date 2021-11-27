4824c9972e7abde5ee7e76f78ec6c63bb525f820
Joung Young Ho <dudgh0314@gmail.com>
2021-11-27 21:09:18 +0900

    feat: show myInfo

--- index.js
+++ index.js
@@ -12,3 +12,14 @@ function setDescLength() {
     document.querySelector("span.desclength").innerHTML 
     = document.querySelector("input.description").value.length +"/20";
 }
+
+function showMyInfo(){
+    document.querySelector("#myInfoId").innerHTML = my_info["id"];
+    document.querySelector("#myInfoUserName").innerHTML = my_info.user_name;
+    document.querySelector("#ip-intro").value = my_info.introduction;
+    document.querySelector("#sp-intro").innerHTML = my_info.introduction;
+}
+
+function init() {
+    showMyInfo();
+}
